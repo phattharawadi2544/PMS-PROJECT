2efaaa4e27b7e0aa4280a214f02b9487b21cd05e
phattharawadi2544 <86403649+phattharawadi2544@users.noreply.github.com>
2023-02-24 16:45:51 +0700

    แก้โฟร์เดอร์ใหม่

--- app/Config/App.php
+++ app/Config/App.php
@@ -22,7 +22,7 @@ class App extends BaseConfig
      * explicitly and never rely on auto-guessing, especially in production
      * environments.
      */
-    public string $baseURL = 'http://localhost/project/PMS/';
+    public string $baseURL = 'http://localhost/project/PMSV2/PMS-PROJECT/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
