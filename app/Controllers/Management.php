4bad29d856bfc889d03bd586cc761af27dc133fd
Chonthicha99 <86400506+Chonthicha99@users.noreply.github.com>
2023-03-02 12:17:30 +0700

    แก้ข้อมความลบ

--- app/Controllers/Management.php
+++ app/Controllers/Management.php
@@ -24,10 +24,10 @@ class Management extends BaseController
         view('template/footer.php');
     }
     //delete_user
-    public function delete_user()
+    public function delete_pharmacies_type()
     {
         //  var_dump($_POST);
-        $id = $_POST['userID'];
+        $id = $_POST['id_pharmacy_type'];
         $data = array(
             'status'=>0, 
         );
@@ -36,7 +36,7 @@ class Management extends BaseController
 
         $session = session();
         $session->setFlashdata('message_session', '201');
-        return redirect()->to('/pharmacy_type');
+        return redirect()->to('/pharmacy_type.php');
 
     }
 
@@ -48,6 +48,21 @@ class Management extends BaseController
         return view('template/header.php').
         view('basic_management/recive_type.php',$data).
         view('template/footer.php');
+    }
+    public function delete_recive_type()
+    {
+        //  var_dump($_POST);
+        $id = $_POST['recive_type_id'];
+        $data = array(
+            'status'=>0, 
+        );
+        $model = new Pharmacy_typeModel();
+        $model->where('recive_type_id',$id)->set($data)->update();
+
+        $session = session();
+        $session->setFlashdata('message_session', '202');
+        return redirect()->to('/recive_type.php');
+
     }
 
     // public function user_role()
--- app/Views/basic_management/pharmacy_type.php
+++ app/Views/basic_management/pharmacy_type.php
@@ -120,22 +120,22 @@
     <div class="modal-dialog">
         <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title">เติลอี้ลบหมันไม้</h5>
+            <h5 class="modal-title">ยืนยันการลบข้อมูล</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-        <p>ข้อมูลผู้ใช้งานที่มี</p>
+        <p>ข้อมูลประเภทยา</p>
         <form class="row g-3" id="form_pharmacy_type_del" method="post" action="<?php echo site_url('del_pharmacy_type'); ?>">
         <div class="col-md-12">
-                            <label for="del_id_pharmacy_type" class="form-label">รหัสผู้ใช้งาน </label>
+                            <label for="del_id_pharmacy_type" class="form-label">รหัสผู้ใช้งาน</label>
                             <input type="text" class="form-control was-validated" id="del_id_pharmacy_type" name="id_pharmacy_type" onchange="clr_border(this);" readonly>
                         </div>
         </form>
             
         </div>
         <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary" onclick="save_del_user()">Save changes</button>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">ยกเลิก</button>
+            <button type="button" class="btn btn-primary" onclick="save_del_user()">ยืนยันการลบ</button>
         </div>
         </div>
     </div>
