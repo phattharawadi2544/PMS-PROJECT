2cbe85e05f2f44cc0c8aa69d2d14e330f32c9e1c
phattharawadi2544 <86403649+phattharawadi2544@users.noreply.github.com>
2023-03-08 17:55:20 +0700

    ขายยยย

--- app/Views/sale/sale_test.php
+++ app/Views/sale/sale_test.php
@@ -1,16 +1,137 @@
-<div class="content-page">
-    <div class="container-fluid">
+        
+        
+     <div class="content-page">
+        <div class="container-fluid">   
         <div class="row">
-            <div class="col-lg-12">
-                <div class="d-flex flex-wrap align-items-center justify-content-between mb-4">
-                    <div>
-                        <h4 class="mb-3">รายการขายทั้งหมด</h4>
-                       
+            <div class="col-lg-2">
+                <div class="card card-transparent card-block card-stretch card-height border-none">
+                    <div class="card-body p-0 mt-lg-2 mt-0">
+                        <h3 class="mb-3">สร้างใบสั่งซื้อ</h3>
+                        <!-- <p class="mb-0 mr-4">Your dashboard gives you views of key performance or business process.</p> -->
                     </div>
                 </div>
             </div>
-            
+            <div class="col-lg-10">
+                <div class="card auth-card">
+                     <div class="card-body p-0">
+                        <div class="d-flex align-items-center auth-content">
+                           <div class="col-lg-12 align-self-center">
+                              <div class="p-3">
+                                <h6 class="card-title">ข้อมูลผู้ซื้อ</h6>
+                                <form>
+                                    <div class="form-row">
+                                    <div class="col-4">
+                                        <input type="text" class="form-control" placeholder="ชื่อลูกค้า">
+                                    </div>
+                                    <div class="col-6">
+                                        <input type="text" class="form-control" placeholder="ที่อยู่">
+                                    </div>
+                                    <div class="col-2">
+                                        <input type="text" class="form-control" placeholder="เลขผู้เสียภาษี">
+                                    </div>
+                                    </div>
+                                </form>
+                              </div>
+                           </div>
+                          
+                        </div>
+                     </div>
+                  </div>
+            </div>
+            <div class="col-lg-8">
+                <div class="card auth-card">
+                     <div class="card-body p-0">
+                        <div class="d-flex align-items-center auth-content">
+                           <div class="col-lg-12 align-self-center">
+                              <div class="p-3">
+                                <h6 class="card-title">ข้อมูลผู้ซื้อ</h6>
+                                <table class="table">
+                                    <thead>
+                                    <tr class="ligth">
+                                        <th scope="col">#</th>
+                                        <th scope="col">First</th>
+                                        <th scope="col">Last</th>
+                                        <th scope="col">Handle</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td>Larry</td>
+                                        <td>the Bird</td>
+                                        <td>@twitter</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+
+
+                              </div>
+                           </div>
+                          
+                        </div>
+                     </div>
+                  </div>
+            </div>
+            <div class="col-lg-4">
+            <div class="card auth-card">
+                     <div class="card-body p-0">
+                        <div class="d-flex align-items-center auth-content">
+                           <div class="col-lg-12 align-self-center">
+                              <div class="p-3">
+                                <h6 class="card-title">ข้อมูลผู้ซื้อ</h6>
+                                <table class="table">
+                                    <thead>
+                                    <tr class="ligth">
+                                        <th scope="col">#</th>
+                                        <th scope="col">First</th>
+                                        <th scope="col">Last</th>
+                                        <th scope="col">Handle</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td>Larry</td>
+                                        <td>the Bird</td>
+                                        <td>@twitter</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+
+
+                              </div>
+                           </div>
+                          
+                        </div>
+                     </div>
+                  </div>
+            </div>
+             
+        </div>  
+    <!-- Page end  -->
     </div>
-</div>
-   
-   
+      </div>
