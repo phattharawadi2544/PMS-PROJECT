d0c43c4d28bad393b443deb99684f0379bc4d58e
phattharawadi2544 <86403649+phattharawadi2544@users.noreply.github.com>
2023-03-10 11:20:11 +0700

    ค้นหา

--- app/Views/sale/sale_test.php
+++ app/Views/sale/sale_test.php
@@ -107,12 +107,13 @@
                      </div>
                   </div>
             </div>
+            
             <div class="col-lg-4">
                 <div class="card auth-card">
                      <div class="card-body p-0">
                         <div class="d-flex align-items-center auth-content">
                            <div class="col-lg-12 align-self-center">
-                              <div class="p-3">
+                              <!-- <div class="p-3">
                                 <h6 class="card-title">รายการที่ต้องการขาย</h6>
                                 <table class="table" class="table-sm">
                                     <thead>
@@ -141,7 +142,7 @@
                                         </td>
                                     </tr>
                                     </tbody>
-                                </table>
+                                </table> -->
 
                                     
                                         <h6 class="card-title">ยอดรวมสุทธิ</h6>
@@ -170,7 +171,94 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+      <div class="iq-search-bar device-search">
+                      <form action="#" class="searchbox">
+                          <a class="search-link" href="#"><i class="ri-search-line"></i></a>
+                          <input type="text" class="text search-input" placeholder="Search here...">
+                      </form>
+                  </div>
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>How To Add and Delete Rows Dynamically Using jQuery - Techsolutionstuff</title>
+<style>
+    form{
+        margin: 20px;
+    }
+    form input, button{
+        padding: 5px;
+    }
+    table{
+        width: 90%;
+        margin: 20px;
+		border-collapse: collapse;
+    }
+    table, th, td{
+        border: 1px solid #cdcdcd;
+    }
+    table th, table td{
+        padding: 10px;
+        text-align: left;      	
+    }
+  	.delete-row, h2{
+      margin:20px;
+  	}
+</style>
+
+</head>
+<body style="border:1px solid grey">
+  	
+    <!-- <form>
+        <input type="text" id="name" placeholder="Name">
+        <input type="text" id="email" placeholder="Email Address">
+    	<input type="button" class="add-row" value="Add Row">
+    </form> -->
+    <table>
+        <thead>
+            <tr>
+                <th>Select</th>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><input type="checkbox" name="record"></td>
+                <td>techsolutionstuff</td>
+ <td>techsolutionstuff@test.com</td>
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="record"></td>
+                <td>web developemnt</td>
+                <td>web@test.com</td>
+            </tr>
+        </tbody>
+    </table>
+    <button type="button" class="delete-row">Delete Row</button>
+</body> 
+</html>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script>
+    $(document).ready(function(){
+        $(".add-row").click(function(){
+            var name = $("#name").val();
+            var email = $("#email").val();
+            var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + name + "</td><td>" + email + "</td></tr>";
+            $("table tbody").append(markup);
+        });
+        
+        // Find and remove selected table rows
+        $(".delete-row").click(function(){
+            $("table tbody").find('input[name="record"]').each(function(){
+            	if($(this).is(":checked")){
+                    $(this).parents("tr").remove();
+                }
+            });
+        });
+    });    
+</script>
+ 
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">ปิด</button>
